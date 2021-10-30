2ba1ea1ddbf64fda5873bfb7c1b3b5447a5fcfb2
Sepehr Ghasemi <sepehrghasemi1998@gmail.com>
2021-10-30 15:49:58 +0330

    Init

---
+++ componenets/toolbar.js
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router' ;
+import styles from '../styles/Toolbar.module.css' ;
+
+export const Toolbar = () => {
+   const router = useRouter() ;
+    
+   return(
+      <div className={styles.main}>
+         <div onClick={() => router.push('/')}>Home</div>
+         <div onClick={() => router.push('/feed/1')}>feed</div>
+         <div onClick={() => window.location.href = ' https://twitter.com/sewphr'}>twitter</div>
+      </div>
+   );
+
+
+};
